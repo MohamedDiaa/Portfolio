a3208e3ae6801d13533478e3d0106b9182bec7af
Mohamed Alwakil <736736+MohamedDiaa@users.noreply.github.com>
2024-12-11 13:41:02 +0100

    Initial Commit

---
+++ Portifolio/PortifolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortifolioApp.swift
+//  Portifolio
+//
+//  Created by Mohamed Alwakil on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct PortifolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
